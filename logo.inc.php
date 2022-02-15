1fdada0673b61a38ae3050a521fefe76f5bc24bc
Vladimir-Belyak <wabelyak@mail.ru>
2022-02-15 21:01:11 +0700

    1st upload, 3 of 4

---
+++ logo.inc.php
@@ -0,0 +1,5 @@
+
+    
+<div class="logo"> 
+    <img src="img/forest_logo.png" alt="Forest">
+</div>  
---
+++ main.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>11_6_Vladimir_Belyak</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    
+    <div class="flex-container">
+
+        <div class="header">     
+
+            <?php require_once 'menu.inc.php' ?>	    
+            <?php require_once 'logo.inc.php' ?>   
+
+        </div>       
+     
+        <div class="about_me">
+         
+          <h1>  <?php  echo $welcome  ?> </h1>
+
+            <div class="data">
+                <div>
+                    <?php  echo '<img class="myImg" src="img/belochka.jpeg">'; ?>                 
+                </div>
+
+                <div class="fullname">
+                    <p> Меня зовут 
+                    <?php echo $name, ' ', $surname . '<br>'; 
+                          echo 'Место жительства: ', $city; ?>                                      
+                    </p> 
+           
+                    <p> Мне
+                    <?php  echo $age;   ?>          
+                    года </p>
+                    <p class="text">
+                    Белки — милые зверки, которых многие с умилением кормят орешками в парках.
+                    Они широко распространены по всему миру, и везде вызывают у людей только позитивную реакцию.
+                    Пугливые и шустрые, белки отличаются беззлобным нравом.
+                    </p>                    
+                </div>
+            </div>
+
+            <div class="knowledge">
+                                   
+                    <?php  require_once 'knowledge.inc.php'; ?>
+                    <?php   echo $a, ' ', $b, ' ', $c; ?> <br>
+                    <p> Мы научились работать с переменными </p>                
+                    <?php
+                        $a = 10;
+                        $b = 20;
+                        $c = $a + $b;
+                        echo $c;
+                    ?>   <br>                
+                     <?php
+                        echo $d;
+                    ?> 
+
+            </div>
+        </div>
+
+            <?php require_once 'footer.inc.php' ?>
+
+    </div>
+
+
+</body>
+</html>
